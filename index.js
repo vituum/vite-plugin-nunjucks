71d78aefdc29186272eaa8d1b41a406ff767b25d
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2022-08-29 15:07:26 +0200

    fix: json type, reload

--- index.js
+++ index.js
@@ -105,6 +105,9 @@ const plugin = (options = {}) => {
         transformIndexHtml: {
             enforce: 'pre',
             async transform(content, { path, filename, server }) {
+                path = path.replace('?raw', '')
+                filename = filename.replace('?raw', '')
+
                 if (
                     !options.filetypes.html.test(path) &&
                     !options.filetypes.json.test(path) &&
@@ -143,8 +146,8 @@ const plugin = (options = {}) => {
         },
         handleHotUpdate({ file, server }) {
             if (
-                typeof options.reload === 'function' && options.reload(file) ||
-                options.reload && (options.filetypes.html.test(file) || options.filetypes.json.test(file))
+                (typeof options.reload === 'function' && options.reload(file)) ||
+                (typeof options.reload === 'boolean' && options.reload && (options.filetypes.html.test(file) || options.filetypes.json.test(file)))
             ) {
                 server.ws.send({ type: 'full-reload' })
             }
