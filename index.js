4255398333416c12f8869a985d168006f6d1fd7f
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2023-06-26 14:28:37 +0200

    Revert "feat: normalize path"

--- index.js
+++ index.js
@@ -63,9 +63,7 @@ const renderTemplate = async ({ filename, server, resolvedConfig }, content, opt
             })
         }
 
-        context.template = normalizePath(relative(resolvedConfig.root, context.template)).startsWith(normalizePath(relative(resolvedConfig.root, options.root)))
-            ? normalizePath(resolve(resolvedConfig.root, context.template))
-            : normalizePath(resolve(options.root, context.template))
+        context.template = relative(resolvedConfig.root, context.template).startsWith(relative(resolvedConfig.root, options.root)) ? resolve(resolvedConfig.root, context.template) : resolve(options.root, context.template)
     } else if (fs.existsSync(`${initialFilename}.json`)) {
         lodash.merge(context, JSON.parse(fs.readFileSync(`${initialFilename}.json`).toString()))
     }
@@ -129,8 +127,6 @@ const plugin = (options = {}) => {
 
             if (!options.root) {
                 options.root = config.root
-            } else {
-                options.root = normalizePath(options.root)
             }
         },
         buildStart: async () => {
